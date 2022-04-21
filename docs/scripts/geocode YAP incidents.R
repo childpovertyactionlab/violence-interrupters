d2d231eee37d376ee23e4bd41f4b0a9687154eae
Michael Lopez <michael@childpovertyactionlab.org>
2022-04-21 16:27:24 -0500

    added point data

---
+++ docs/scripts/geocode YAP incidents.R
@@ -0,0 +1,21 @@
+library(tidygeocoder)
+library(tidyverse)
+library(sf)
+library(rio)
+
+shootingIncidents <- import("data/YAP_Incidents.xlsx", which = "shooting incident") %>%
+  janitor::clean_names(.) %>%
+  geocode(address = address,
+          method = "arcgis") %>%
+  st_as_sf(coords = c("long", "lat"),
+           crs = 4269)
+
+communityEvent <- import("data/YAP_Incidents.xlsx", which = "community event") %>%
+  janitor::clean_names(.) %>%
+  geocode(address = address,
+          method = "arcgis") %>%
+  st_as_sf(coords = c("long", "lat"),
+           crs = 4269)
+
+st_write(shootingIncidents, "data/YAP Shooting Incidents.geojson")
+st_write(communityEvent, "data/YAP Community Events.geojson")
